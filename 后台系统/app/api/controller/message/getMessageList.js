896dec93293695c4fa2cda36e47c41c2aa892915
yang.yang <986699180@qq.com>
2020-04-04 23:08:33 +0800

    DEV-消息模块-首页消息列表

--- 后台系统/app/api/controller/message/getMessageList.js
+++ 后台系统/app/api/controller/message/getMessageList.js
@@ -1,21 +1,58 @@
 var mongoose = require('mongoose');
 var common = require('../common');
 var message = mongoose.model('message');
+var user = mongoose.model('user');
 
 module.exports = function(req,res) {
-	var params = req.query;
-	if(!params.sender || !params.reciever || !params.message || !params.time) {
-		common.sendJsonResponse(res, 500, 0,"参数不足");
+	let {ID} = req.query;
+	if(!ID) {
+		common.sendJsonResponse(res, 500, 1,"ID不能为空");
 		return;
 	}
-	var data = new message({sender: params.sender,reciever: params.reciever,message: params.message,time: params.time});
-	data.save(function (err) {
-	  if(err) {
-	  	console.log(err);
-	  	common.sendJsonResponse(res, 500, 0,err);
-	  	return;
-	  }
-	  common.sendResponse(res, 200, 0, "消息发送成功");
-	  // saved!
-	})
+	message.find({$or: [{sender:ID},{reciever:ID}]},   //先查一下消息列表
+	    function(err, data){
+	        if(err) {
+	        	console.log(err);
+	        	common.sendJsonResponse(res, 500, 1,err);
+	        	return;
+	        }
+			let arr = [];
+			data.forEach((item,index)=>{
+				let status = true;
+				arr.forEach((ele)=>{
+					if(ele.ID === item.sender || ele.ID === item.reciever) {
+					   ele.data = item.message;
+					   status = false;
+					}
+				})
+				if(!status) {
+					return
+				}
+				if(ID === item.sender)
+				{
+					arr.push({ID:item.reciever,data:item.message,time:item.time});
+				} else {
+					arr.push({ID:item.sender,data:item.message,time:item.time});
+				}
+			})
+			arr.forEach((item,index)=>{                     //在单独查一下名字
+			    new Promise((resolve,reject)=>{
+					user.find({ID: item.ID}, function(err,data) {
+						if(err) {
+							console.log(err);
+							common.sendJsonResponse(res, 500, 0,err);
+						}
+						item = {...item,name:data[0].name} ,
+						resolve(item);
+					})
+				}).then((value)=>{
+					arr[index] = value;
+					if(index == arr.length-1) {
+						common.sendResponse(res, 200, 0, "详细消息列表获取成功",arr);
+					}
+				})
+				
+			})
+	        
+	    })
 }
